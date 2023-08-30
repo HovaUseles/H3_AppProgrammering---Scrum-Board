fb1085bb7c25ee9392b93283f80094db4eb7caa9
HovaUseless <88477974+HovaUseles@users.noreply.github.com>
2023-08-30 19:03:53 +0200

    switched to cacheservice

--- app_scrum_board/lib/data_access/scrum_card_data_handler.dart
+++ app_scrum_board/lib/data_access/scrum_card_data_handler.dart
@@ -1,7 +1,7 @@
-
+import 'dart:async';
 import 'dart:convert';
 
-import 'package:localstore/localstore.dart';
+import 'package:app_scrum_board/data_access/cache_service.dart';
 
 import '../exceptions/_exceptions.dart';
 import '../models/_models.dart';
@@ -10,22 +10,24 @@ import 'api_client.dart';
 class ScrumCardDataHandler {
   final _apiClient =
       ApiClient(ApiClientType.secure, baseUrl: "https://10.0.2.2:5002/api");
-
-  final db = Localstore.instance;
-
   final String _entityContext = "ScrumCard";
-  
+  final CacheService _cacheService = CacheService(entityContext: "ScrumCard");
+
   /// Gets all the items from external API
   Future<List<ScrumCard>> getAll() async {
     try {
       var response = await _apiClient.get(_entityContext);
-      
+
       String responseBody = await response.transform(utf8.decoder).join();
       Iterable jsonArray = json.decode(responseBody);
-      List<ScrumCard> scrumCards = List<ScrumCard>.from(
-        jsonArray.map((jsonObject) => ScrumCard.fromMap(jsonObject)) // Turn json array into Gallery Item objects
-        );
-      
+      List<ScrumCard> scrumCards = List<ScrumCard>.from(jsonArray.map(
+              (jsonObject) => ScrumCard.fromMap(
+                  jsonObject)) // Turn json array into Gallery Item objects
+          );
+
+      await _cacheService.setCache(responseBody); // Set Cache
+
+      /*
       // Caching items to local db, to enable offline use
       await db.collection(_entityContext).delete(); // Empty existing cache
       for (int i = 0; i < scrumCards.length; i++) {
@@ -35,10 +37,24 @@ class ScrumCardDataHandler {
         // Save to local storage
         await db.collection(_entityContext).doc(id).set(map);
       }
+      */
       return scrumCards;
     }
-    // on(TimeoutException, ) {
+    on(TimeoutException, ) {
+      String? localCache = await _cacheService.get();
+      if (localCache != null) {
+        Iterable jsonArray = json.decode(localCache);
+        List<ScrumCard> scrumCardsCache = List<ScrumCard>.from(jsonArray.map(
+                (jsonObject) => ScrumCard.fromMap(
+                    jsonObject)) // Turn json array into Gallery Item objects
+            );
+        return scrumCardsCache;
+      }
+      return []; // If no cached data, return empty lists
+    }
     catch (ex) {
+      rethrow;
+      /*
       // Get data from local storage
       var localItems = await db.collection(_entityContext).get();
       if (localItems == null) {
@@ -51,6 +67,7 @@ class ScrumCardDataHandler {
         scrumCards.add(ScrumCard.fromMap(map));
       }
       return scrumCards; // Return cached data
+      */
     }
   }
 
@@ -67,20 +84,21 @@ class ScrumCardDataHandler {
     Map<String, dynamic> jsonBody = scrumCard.toMap();
     var response = await _apiClient.post(_entityContext, jsonBody);
     String responseBody = await response.transform(utf8.decoder).join();
-    if(responseBody.isNotEmpty) {
+    if (responseBody.isNotEmpty) {
       try {
-        ScrumCard createdScrumCard = ScrumCard.fromJson(responseBody); // Should return the created model
+        ScrumCard createdScrumCard =
+            ScrumCard.fromJson(responseBody); // Should return the created model
         return createdScrumCard;
-      }
-      catch (ex) {
+      } catch (ex) {
         rethrow;
       }
     }
 
     // Check for Uri header
     String? uri = response.headers.value("location");
-    if(uri == null) {
-      throw MissingUriException(message: "Missing URI for created ${(ScrumCard).toString()}");
+    if (uri == null) {
+      throw MissingUriException(
+          message: "Missing URI for created ${(ScrumCard).toString()}");
     }
     // Get created item from the location if not null.
     return await get(uri);
@@ -95,13 +113,15 @@ class ScrumCardDataHandler {
   /// Updates an items on external API
   Future<bool> edit(ScrumCard scrumCardChanges) async {
     Map<String, dynamic> jsonBody = scrumCardChanges.toMap();
-    await _apiClient.put("$_entityContext/${scrumCardChanges.id}", jsonBody); // Saving response for easy extension refactor of the method
+    await _apiClient.put("$_entityContext/${scrumCardChanges.id}",
+        jsonBody); // Saving response for easy extension refactor of the method
     return true;
   }
-  
+
   /// Deletes an items on external API
   Future<bool> delete(String id) async {
-    await _apiClient.delete("$_entityContext/$id"); // Saving response for easy extension refactor of the method
+    await _apiClient.delete(
+        "$_entityContext/$id"); // Saving response for easy extension refactor of the method
     return true;
   }
 }
